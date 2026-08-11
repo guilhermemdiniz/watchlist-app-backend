d196e47768efbb83ccec064a2c325969fcaeb41b
Guilherme <guidiniz2019.18@gmail.com>
2026-08-11 10:34:05 -0300

    remoção de logs da vercel

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,4 @@
+// src/config/database.ts
 import mongoose from 'mongoose';
 
 let isConnected = false;
@@ -10,25 +11,16 @@ export const connectDB = async () => {
 
   const mongoUri = process.env.MONGO_URI;
 
-  // 1. Usando console.error para FORÇAR a exibição nos logs da Vercel
-  console.error(
-    "STRING RECEBIDA NA VERCEL:", 
-    mongoUri ? JSON.stringify(mongoUri.replace(/:([^@]+)@/, ":****@")) : "VARIÁVEL INDEFINIDA (UNDEFINED)"
-  );
-
   if (!mongoUri) {
     throw new Error('A variável de ambiente MONGO_URI não está definida.');
   }
 
   try {
     const db = await mongoose.connect(mongoUri);
-
-    isConnected = db.connections[0].readyState === 1;
+    
+    isConnected = db.connections[0].readyState === 1; 
     console.log('🚀 Conectado com sucesso ao MongoDB');
   } catch (error) {
-    // console.error('❌ Erro ao conectar no MongoDB:', error);
-    console.error('❌ [CODIGO-NOVO] Erro ao conectar no MongoDB:', error);
-    // 2. Lançar o erro para interromper a requisição corretamente
-    throw error;
+    console.error('❌ Erro ao conectar no MongoDB:', error);
   }
 };
